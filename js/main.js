fccc8ec4f333d9f9a48dfa17d6312f5ded57a5cf
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2022-05-28 23:37:48 -0500

    use seconds option

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 import { updateClock } from "./clock.js";
 
-document.addEventListener("DOMContentLoaded", setFormat);
+document.addEventListener("DOMContentLoaded", () => {
+    setFormat();
+    setSeconds();
+});
+
 window.requestAnimationFrame(updateClock);
 
 const formatSwitchButton = document.getElementById("formatSwitchButton");
+const useSecondsButton = document.getElementById("useSecondsButton");
+
 formatSwitchButton.addEventListener("click", changeFormat);
+useSecondsButton.addEventListener("click", changeSeconds);
 
 let format = localStorage.getItem("7_Segment_Clock_Format") || "12h";
 
@@ -18,10 +25,44 @@ function changeFormat() {
             break;
     }
 
-    formatSwitchButton.textContent = format;
+    setFormat();
     localStorage.setItem("7_Segment_Clock_Format", format);
 }
 
 function setFormat() {
+    if (format === "24h") {
+        formatSwitchButton.style.setProperty("--hue", 110);
+    } else {
+        formatSwitchButton.style.setProperty("--hue", null);
+    }
+
     formatSwitchButton.textContent = format;
+}
+
+let useSeconds = localStorage.getItem("7_Segment_Clock_Use_Seconds") || "Yes";
+
+function changeSeconds() {
+    switch (useSeconds) {
+        case "Yes":
+            useSeconds = "No"
+            break;
+        case "No":
+            useSeconds = "Yes"
+            break;
+    }
+
+    setSeconds();
+    localStorage.setItem("7_Segment_Clock_Use_Seconds", useSeconds);
+}
+
+function setSeconds() {
+    if (useSeconds === "No") {
+        useSecondsButton.style.setProperty("--hue", 15);
+        useSecondsButton.style.color = "white";
+    } else {
+        useSecondsButton.style.setProperty("--hue", null);
+        useSecondsButton.style.color = "";
+    }
+
+    useSecondsButton.textContent = useSeconds;
 }
